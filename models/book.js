dccea6ca0c5d536296359ad5a9498acf9f0603ea
Yonatan Melka <ybmelka2@gmail.com>
2020-01-24 15:58:52 -0600

    Setup books.js rout

--- models/book.js
+++ models/book.js
@@ -5,8 +5,22 @@ module.exports = sequelize => {
   class Book extends Sequelize.Model {}
   Book.init(
     {
-      title: Sequelize.STRING,
-      author: Sequelize.STRING,
+      title: {
+        type: Sequelize.STRING,
+        validate: {
+          notEmpty: {
+            msg: '"Title" is required'
+          }
+        }
+      },
+      author: {
+        type: Sequelize.STRING,
+        validate: {
+          notEmpty: {
+            msg: '"Author" is required'
+          }
+        }
+      },
       genre: Sequelize.STRING,
       year: Sequelize.INTEGER
     },
