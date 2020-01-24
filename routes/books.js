0b20ad88630698ca40ccb4e04095c26ed21c2bbe
Yonatan Melka <ybmelka2@gmail.com>
2020-01-24 16:57:39 -0600

    Create index.pug under books

--- routes/books.js
+++ routes/books.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 const Book = require('../models').Book;
 
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll({ order: [['title', 'ASC']] });
+    res.render('books/index', { books });
+  })
+);
 
 module.exports = router;
