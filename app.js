8da3c3655beb118e37a57e7f1edc143e56d61ab6
Yonatan Melka <ybmelka2@gmail.com>
2020-01-30 00:14:08 -0600

    Add database connection test

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const { sequelize } = require('./models');
+
 const indexRouter = require('./routes/index');
 const booksRouter = require('./routes/books');
 
@@ -18,6 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// test the connection to the database.
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database is successfull~');
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
 
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
